662082f1fae73cd6bc022a14cf0cd99ec0fa4ecd
iliko13 <107374131+iliko13@users.noreply.github.com>
2023-08-12 17:08:12 +0400

    Last commit

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,74 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+
+    updateName(state, action) {
+      state.fullName = action.payload.fullName;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+// export default function customerReducer(state = initialStateCostumer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createdAt: action.payload.createdAt,
+//       };
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// export const createCustomer = (fullName, nationalID) => {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+//   };
+// };
+
+// export const updateName = (fullName, nationalID) => {
+//   return {
+//     type: "customer/updateName",
+//     payload: { fullName, nationalID },
+//   };
+// };
